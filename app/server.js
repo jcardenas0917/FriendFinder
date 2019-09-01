512c9e06d70ff2c2f3cd499fb849426b67b88f89
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-09-01 19:21:00 -0400

    working on uploading to heroku

--- app/server.js
+++ app/server.js
@@ -12,9 +12,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-});
+app.listen(PORT, () => console.log("App listening on PORT " + PORT));
 
 // Requires and sets the HTML routes in this file
 require('../app/routing/htmlRoutes')(app);
