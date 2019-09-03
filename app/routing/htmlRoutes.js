40f29ad0c87e746b46d45803a8e155e563a69c93
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-09-03 11:53:07 -0400

    deploy

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require("path");
 
+
 module.exports = app =>{
     app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../public/home.html")));
 
